c313aa5560fbe8aa87214d95dde924f596d19539
Jerry Wiltse <jerrywiltse@gmail.com>
2017-08-01 17:20:51 -0400

    added package info header-only flag

--- conanfile.py
+++ conanfile.py
@@ -5,14 +5,32 @@ class BoostLockfreeConan(ConanFile):
     version = "1.64.0"
     generators = "txt"
     url = "https://github.com/boostorg/lockfree"
-    description = "For a description of this library, please visit http://boost.org/lockfree "
+    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_name = "lockfree"
+    requires =  "Boost.Align/1.64.0@bincrafters/testing", \
+                      "Boost.Array/1.64.0@bincrafters/testing", \
+                      "Boost.Assert/1.64.0@bincrafters/testing", \
+                      "Boost.Atomic/1.64.0@bincrafters/testing", \
+                      "Boost.Config/1.64.0@bincrafters/testing", \
+                      "Boost.Core/1.64.0@bincrafters/testing", \
+                      "Boost.integer/1.64.0@bincrafters/testing", \
+                      "Boost.Mpl/1.64.0@bincrafters/testing", \
+                      "Boost.Parameter/1.64.0@bincrafters/testing", \
+                      "Boost.Predef/1.64.0@bincrafters/testing", \
+                      "Boost.Static_Assert/1.64.0@bincrafters/testing", \
+                      "Boost.Type_Traits/1.64.0@bincrafters/testing", \
+                      "Boost.Utility/1.64.0@bincrafters/testing"
 
+                      #align3 array3 assert1 atomic4 config0 core2 integer3 mpl5 parameter10 predef0 static_assert1 tuple4 type_traits3 utility5
+                      
     def source(self):
         self.run("git clone --depth=50 --branch=boost-{0} {1}.git"
                  .format(self.version, self.url))
 
     def package(self):
-        include_dir = src=os.path.join(os.getcwd(), self.lib_short_name, "include")
+        include_dir = os.path.join(self.build_folder, self.lib_short_name, "include")
         self.copy(pattern="*", dst="", src=include_dir)
+
+    def package_id(self):
+        self.info.header_only()
