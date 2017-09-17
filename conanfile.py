95ac4c8e1f6c45badcc846bfe085a9e7dd26f917
Jerry Wiltse <jerrywiltse@gmail.com>
2017-09-17 11:33:06 -0400

    Updated version to 1.65.1

--- conanfile.py
+++ conanfile.py
@@ -2,25 +2,25 @@ from conans import ConanFile, tools, os
 
 class BoostLockfreeConan(ConanFile):
     name = "Boost.Lockfree"
-    version = "1.64.0"
+    version = "1.65.1"
     short_paths = True
     url = "https://github.com/bincrafters/conan-boost-lockfree"
-    description = "Please visit http://www.boost.org/doc/libs/1_64_0/libs/libraries.htm"
+    description = "Please visit http://www.boost.org/doc/libs/1_65_1/libs/libraries.htm"
     license = "www.boost.org/users/license.html"
     lib_short_names = ["lockfree"]
-    requires =  "Boost.Align/1.64.0@bincrafters/testing", \
-                      "Boost.Array/1.64.0@bincrafters/testing", \
-                      "Boost.Assert/1.64.0@bincrafters/testing", \
-                      "Boost.Atomic/1.64.0@bincrafters/testing", \
-                      "Boost.Config/1.64.0@bincrafters/testing", \
-                      "Boost.Core/1.64.0@bincrafters/testing", \
-                      "Boost.Integer/1.64.0@bincrafters/testing", \
-                      "Boost.Mpl/1.64.0@bincrafters/testing", \
-                      "Boost.Parameter/1.64.0@bincrafters/testing", \
-                      "Boost.Predef/1.64.0@bincrafters/testing", \
-                      "Boost.Static_Assert/1.64.0@bincrafters/testing", \
-                      "Boost.Type_Traits/1.64.0@bincrafters/testing", \
-                      "Boost.Utility/1.64.0@bincrafters/testing"
+    requires =  "Boost.Align/1.65.1@bincrafters/testing", \
+                      "Boost.Array/1.65.1@bincrafters/testing", \
+                      "Boost.Assert/1.65.1@bincrafters/testing", \
+                      "Boost.Atomic/1.65.1@bincrafters/testing", \
+                      "Boost.Config/1.65.1@bincrafters/testing", \
+                      "Boost.Core/1.65.1@bincrafters/testing", \
+                      "Boost.Integer/1.65.1@bincrafters/testing", \
+                      "Boost.Mpl/1.65.1@bincrafters/testing", \
+                      "Boost.Parameter/1.65.1@bincrafters/testing", \
+                      "Boost.Predef/1.65.1@bincrafters/testing", \
+                      "Boost.Static_Assert/1.65.1@bincrafters/testing", \
+                      "Boost.Type_Traits/1.65.1@bincrafters/testing", \
+                      "Boost.Utility/1.65.1@bincrafters/testing"
 
                       #align3 array3 assert1 atomic4 config0 core2 integer3 mpl5 parameter10 predef0 static_assert1 tuple4 type_traits3 utility5
                       
