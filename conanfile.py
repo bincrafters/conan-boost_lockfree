be25c428f6a91fb0283a69d3a9ecb137f2d75911
Jerry W <jerrywiltse@gmail.com>
2018-01-25 01:12:37 -0500

    Refresh round 2

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,8 @@ class BoostLockfreeConan(ConanFile):
     name = "boost_lockfree"
     version = "1.66.0"
     url = "https://github.com/bincrafters/conan-boost_lockfree"
-
+    author = "Bincrafters <bincrafters@gmail.com>"
+    exports = ["LICENSE.md"]
     lib_short_names = ["lockfree"]
     is_header_only = True
 
