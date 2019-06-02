4b06fd480ec071b5cc87bbcb0fb8a786d16f953b
Rene Rivera <grafikrobot@gmail.com>
2019-06-02 01:13:05 -0500

    Boost 1.70.0 [skip ci]

--- conanfile.py
+++ conanfile.py
@@ -4,28 +4,9 @@
 from conans import python_requires
 
 
-base = python_requires("boost_base/1.69.0@bincrafters/testing")
+base = python_requires("boost_base/2.0.0@bincrafters/testing")
+
 
 class BoostLockfreeConan(base.BoostBaseConan):
     name = "boost_lockfree"
-    version = "1.69.0"
-    url = "https://github.com/bincrafters/conan-boost_lockfree"
-    lib_short_names = ["lockfree"]
-    header_only_libs = ["lockfree"]
-    b2_requires = [
-        "boost_align",
-        "boost_array",
-        "boost_assert",
-        "boost_atomic",
-        "boost_config",
-        "boost_core",
-        "boost_integer",
-        "boost_iterator",
-        "boost_mpl",
-        "boost_parameter",
-        "boost_predef",
-        "boost_static_assert",
-        "boost_tuple",
-        "boost_type_traits",
-        "boost_utility"
-    ]
+    version = "1.70.0"
